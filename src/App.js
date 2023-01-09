51222edf31bc2f94752218982f8fbe1a02858a06
Kyoka Miyamura <kyoka.miyamura21@gmail.com>
2023-01-08 19:52:16 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, SearchFeed, CategoryDetail } from "./components";
+
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#fff" }}>
+        <Navbar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <Feed
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+              />
+            }
+          />
+          <Route
+            path="/category/:selectedCategory"
+            element={<CategoryDetail />}
+          />
+          <Route
+            path="/search/:searchTerm"
+            element={
+              <SearchFeed
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+              />
+            }
+          />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
+
+export default App;
